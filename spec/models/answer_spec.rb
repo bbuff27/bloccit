e86b511d5ee26155323d33044ddafadc4a64a4c7
Bill Buff <bbuff27@gmail.com>
2018-03-30 19:47:40 -0600

    Create Question and Answer model tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) {Question.create!(title: "New Question", body: "New Question body", resolved: true)}
+  let(:answer) {Answer.create!(body: "New Answer body", question: question)}
+
+  describe "attributes" do
+    it "has body attribute" do
+      expect(answer).to have_attributes(body: "New Answer body")
+    end
+  end
 end
