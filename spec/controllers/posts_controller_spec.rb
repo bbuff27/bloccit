e7fae35bc871a475d32df42515c6c0178fd17624
Bill Buff <bbuff27@gmail.com>
2018-04-02 17:30:11 -0600

    GET post #show

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "GET #index" do
+  describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
@@ -15,12 +15,22 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: { id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: { id: my_post.id }
+      expect(response).to render_template(:show)
+    end
+
+    it "assignes my_post to @post" do
+      get :show, params: { id: my_post.id }
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
 
   describe "GET new" do
     it "returns http success" do
@@ -55,7 +65,7 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  # describe "GET #edit" do
+  # describe "GET edit" do
   #   it "returns http success" do
   #     get :edit
   #     expect(response).to have_http_status(:success)
