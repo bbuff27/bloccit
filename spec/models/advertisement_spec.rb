97da92d72e310ca16625dd7a1ecc32e6565f037f
Bill Buff <bbuff27@gmail.com>
2018-04-02 18:17:51 -0600

    Add model tests

--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Advertisement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:advertisement) { Advertisement.create!(title: "New Ad", copy: "New copy", price: 10) }
+
+  describe "attributes" do
+    it "has title, copy, and price attributes" do
+      expect(advertisement).to have_attributes(title: "New Ad", copy: "New copy", price: 10)
+    end
+  end
 end
