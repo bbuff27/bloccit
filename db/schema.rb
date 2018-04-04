56ee1a1db1cc5a518b5acf1117e7b2a9c34a0372
Bill Buff <bbuff27@gmail.com>
2018-04-03 18:58:39 -0600

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331004032) do
+ActiveRecord::Schema.define(version: 20180404003805) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -24,6 +32,14 @@ ActiveRecord::Schema.define(version: 20180331004032) do
     t.string "title"
     t.string "string"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
