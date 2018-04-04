ee839d02fcee5580418cf8eb1e1ff64216a692f5
Bill Buff <bbuff27@gmail.com>
2018-04-03 20:59:42 -0600

    Generate and test Question model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
+  get 'questions/new'
+
+  get 'questions/show'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
+  get 'questions/create'
+
   resources :posts
 
   get 'about' => 'welcome#about'
