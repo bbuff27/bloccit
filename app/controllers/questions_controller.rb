012685559f9d8e438016aa73754de7e05d8fa60e
Bill Buff <bbuff27@gmail.com>
2018-04-05 09:47:31 -0600

    #update action and tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,6 +16,17 @@ class QuestionsController < ApplicationController
   end
 
   def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+    if @question.save
+      flash[:notice] = "Question was successfully saved."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :new
+    end
   end
 
   def destroy
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -71,19 +71,26 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "PUT #update" do
+    it "updates question with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
 
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+      put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: true} }
+      updated_question = assigns(:question)
+
+      expect(updated_question.id).to eq(my_question.id)
+      expect(updated_question.title).to eq(new_title)
+      expect(updated_question.body).to eq(new_body)
+    end
+
+    it "redirects to the updated question" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      put :update, params: { id: my_question.id, question: {title: new_title, body: new_body, resolved: true} }
+      expect(response).to redirect_to(my_question)
+    end
+  end
 
   describe "POST #create" do
     it "increases the number of Question by 1" do
