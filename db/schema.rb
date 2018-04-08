d5f3a73662a2b17ea6b121e9ffbb708a90db122e
Bill Buff <bbuff27@gmail.com>
2018-04-08 17:40:07 -0600

    Link key from sponsored posts to topic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408232940) do
+ActiveRecord::Schema.define(version: 20180408233820) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20180408232940) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic_id"
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
