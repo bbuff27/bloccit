c13e4f6bdfc213b14131daf3116456020fc7838e
Bill Buff <bbuff27@gmail.com>
2018-04-11 15:44:22 -0600

    Test/implement User #new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:new_user_attributes) do
+    {
+      name: "BlocHead",
+      email: "user@bloccit.com",
+      password: "blochead",
+      password_confirmation: "blochead"
+    }
+  end
+
+  describe "GET new" do 
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "instantiates a new user" do
+      get :new
+      expect(assigns(:user)).not_to be_nil
+    end
+  end
 
 end
