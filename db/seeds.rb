6e91e4fd8357f8f24e3c03c8eb9ce4b5cf985773
Bill Buff <bbuff27@gmail.com>
2018-04-19 14:23:00 -0600

    Fix seed data error

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ topics = Topic.all
 		body: RandomData.random_paragraph
 	)
 
-post.update(:created_at, rand(10.minutes..1.year).ago)
+post.update_attribute(:created_at, rand(10.minutes..1.year).ago)
 rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample) }
 
 end
